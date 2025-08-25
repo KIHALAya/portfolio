22dfd55e33b4a902134cfdd5020b38b3035da800
KIHALAya <kihalaya00@gmail.com>
2025-08-25 11:22:38 +0100

    delete index.html

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',   // enable static HTML export
+  images: {
+    unoptimized: true,  // since GH pages doesn't support Next.js image optimization
+  },
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio/',
 };
 
 export default nextConfig;
